d4d4a2629b011b2cc10e64ded33a7b1d11a656d2
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-04-07 17:41:10 -0500

    first idk

--- src/main/java/xyz/Controller/GuildController.java
+++ src/main/java/xyz/Controller/GuildController.java
@@ -1,7 +1,10 @@
 package xyz.Controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +22,15 @@ public class GuildController {
 		this.guildService = guildService;
 	}
 
+	@PostMapping
+	public ResponseEntity<String> updateGuildList(@RequestBody List<Guild> guilds) {
+		String responseOutput = guildService.updateGuildList(guilds);
+		return new ResponseEntity<>(responseOutput, HttpStatus.OK);
+	}
+
 	@PutMapping
 	public ResponseEntity<Guild> createGuild(@RequestBody Guild guild) {
 		Guild newGuild = guildService.createGuild(guild);
 		return new ResponseEntity<>(newGuild, HttpStatus.OK);
 	}
-
 }
--- src/main/java/xyz/Services/GuildService.java
+++ src/main/java/xyz/Services/GuildService.java
@@ -1,5 +1,7 @@
 package xyz.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -17,5 +19,21 @@ public class GuildService {
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
 	public Guild createGuild(Guild guild) {
 		return guildRepository.save(guild);
+	}
+
+	public String updateGuildList(List<Guild> guilds) {
+		int updated = 0;
+		int created = 0;
+		for (Guild guild : guilds) {
+			if (guildRepository.existsById(guild.getId())) {
+				// If guild exists already skip
+				updated += 1;
+			} else {
+				// if it doesn't, create it
+				guildRepository.save(guild);
+				created += 1;
+			}
+		}
+		return String.format("Updated(%d) created(%d)", updated, created);
 	}
 }
